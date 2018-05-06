cd727a779ea69d5d577c30856908711925775b9a
wa278 <wa941121@163.com>
2018-05-06 14:14:09 +0800

    Problem 4 Median of Two Sorted Arrays

---
+++ src/MedianOfTwoSortedArrays.java
@@ -0,0 +1,50 @@
+/**
+ * 没做出来 耻辱            对两组数分组，分组的数量有关系。时间复杂度O(log(min(m,n)))
+ */
+public class MedianOfTwoSortedArrays {
+    public double findMedianSortedArrays(int[] A, int[] B) {
+        int m = A.length;
+        int n = B.length;
+        if (m > n) { // to ensure m<=n
+            int[] temp = A;
+            A = B;
+            B = temp;
+            int tmp = m;
+            m = n;
+            n = tmp;
+        }
+        int iMin = 0, iMax = m, halfLen = (m + n + 1) / 2; //如果m+n是偶数，并不改变结果。
+        while (iMin <= iMax) {
+            int i = (iMin + iMax) / 2;
+            int j = halfLen - i;
+            if (i < iMax && B[j - 1] > A[i]) {               // 边界条件就是一组数完全大于或者小于另一组
+                iMin = iMin + 1; // i is too small
+            } else if (i > iMin && A[i - 1] > B[j]) {
+                iMax = iMax - 1; // i is too big
+            } else { // i is perfect
+                int maxLeft = 0;                              //如果是奇数，只找左边最大的就可以
+                if (i == 0) {
+                    maxLeft = B[j - 1];
+                } else if (j == 0) {
+                    maxLeft = A[i - 1];
+                } else {
+                    maxLeft = Math.max(A[i - 1], B[j - 1]);
+                }
+                if ((m + n) % 2 == 1) {
+                    return maxLeft;
+                }
+
+                int minRight = 0;                               // 是偶数找右边最小的取平均数。
+                if (i == m) {
+                    minRight = B[j];
+                } else if (j == n) {
+                    minRight = A[i];
+                } else {
+                    minRight = Math.min(B[j], A[i]);
+                }
+                return (maxLeft + minRight) / 2.0;
+            }
+        }
+        return 0.0;
+    }
+}
