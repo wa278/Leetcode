dd65b02fa4da01002dc4935ec87d890e11325fd0
wa278 <wa941121@163.com>
2018-05-16 09:49:27 +0800

    Problem 20 Valid Parentheses

---
+++ src/ValidParentheses.java
@@ -0,0 +1,35 @@
+import java.util.Stack;
+
+/**
+ * Created by wa941 on 2018/5/16.
+ */
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        Stack<Character> myStack = new Stack<>();
+        for(int i = 0; i < s.length();i++){
+            if(s.charAt(i) == '{' || s.charAt(i) == '[' || s.charAt(i) == '('){
+                myStack.push(s.charAt(i));
+            }
+            else if(s.charAt(i) == ')'){
+                if(myStack.size() == 0 || myStack.pop() != '('){
+                    return false;
+                }
+            }
+            else if(s.charAt(i) == ']'){
+                if(myStack.size() == 0 || myStack.pop() != '['){
+                    return false;
+                }
+            }
+            else if(s.charAt(i) == '}'){
+                if(myStack.size() == 0 || myStack.pop() != '{'){
+                    return false;
+                }
+            }
+        }
+        if(myStack.size() != 0){
+            return false;
+        }
+        else
+            return true;
+    }
+}
