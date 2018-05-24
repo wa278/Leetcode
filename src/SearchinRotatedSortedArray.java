000c0d2eafa80e1edebf46e675698ab28d252f93
wa278 <wa941121@163.com>
2018-05-24 19:31:38 +0800

    Problem 33 Search in Rotated Sorted Array

--- src/SearchinRotatedSortedArray.java
+++ src/SearchinRotatedSortedArray.java
@@ -1,6 +1,6 @@
 /**
  * Created by wa941 on 2018/5/24.
- * 成环之后在某一点分开，总是部分有序的。1左边升序，2右边升序。判断那边有序验证target是否在范围内，是的话在范围内搜索，不是在另一边搜索。
+ * 成环之后在某一点分开，总是部分有序的。1左边升序，2右边升序。判断那边有序验证target是否在范围内，是的话在范围内搜索，不是在另一边搜索。。
  */
 public class SearchinRotatedSortedArray {
     public int search(int[] nums, int target) {
