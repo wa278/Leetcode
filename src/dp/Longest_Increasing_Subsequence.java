c778a7cab3651860df581962284b3099315740c2
frankaowang@tencent.com <wa941121@163.com>
2019-03-24 17:47:56 +0800

    Longest_Increasing_SubSequence

--- src/dp/Longest_Increasing_Subsequence.java
+++ src/dp/Longest_Increasing_Subsequence.java
@@ -6,22 +6,43 @@ dp[i] = if(nums[x] < nums[i]),dp[i] = Max(dp[i],dp[x]+1);
  */
 public class Longest_Increasing_Subsequence {
     public int lengthOfLIS(int[] nums) {
-        if(nums == null || nums.length ==0){
+        if (nums == null || nums.length == 0) {
             return 0;
         }
         int max = 1;
         int[] dp = new int[nums.length];
-        for(int i = 0; i < nums.length; i++){
+        for (int i = 0; i < nums.length; i++) {
             dp[i] = 1;
         }
-        for(int i = 0; i < nums.length; i++){
-            for(int j = 0; j < i; j++){
-                if(nums[j] < nums[i]){
-                    dp[i] = Math.max(dp[j]+1,dp[i]);
-                    max = Math.max(dp[i],max);
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[j] + 1, dp[i]);
+                    max = Math.max(dp[i], max);
                 }
             }
         }
         return max;
     }
 }
+
+
+/*
+public class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        int len = 0;
+        for (int num : nums) {
+            int i = Arrays.binarySearch(dp, 0, len, num);
+            if (i < 0) {
+                i = -(i + 1);
+            }
+            dp[i] = num;
+            if (i == len) {
+                len++;
+            }
+        }
+        return len;
+    }
+}
+ */
