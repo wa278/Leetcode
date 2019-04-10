931f30d98ca3174ad4be2d3597055932399aac86
frankaowang@tencent.com <wa941121@163.com>
2019-04-10 10:32:48 +0800

    binary search

---
+++ src/Binary_Search/MedianOfTwoSortedArrays.java
@@ -0,0 +1,41 @@
+package Binary_Search;
+
+public class MedianOfTwoSortedArrays {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        if (nums1.length > nums2.length) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        int sum = nums1.length + nums2.length;
+        int half = sum / 2;
+        int lo = 0;
+        int hi = nums1.length;
+        int x;
+        int y;
+        while (lo <= hi) {
+            x = lo + (hi - lo) / 2;
+            y = half - x;
+            int maxOfLeftX = x == 0 ? Integer.MIN_VALUE : nums1[x - 1];
+            int maxOfLeftY = y == 0 ? Integer.MIN_VALUE : nums2[y - 1];
+            int minOfRightX = x == nums1.length ? Integer.MAX_VALUE : nums1[x];
+            int minOfRightY = y == nums2.length ? Integer.MAX_VALUE : nums2[y];
+            if (maxOfLeftX <= minOfRightY && maxOfLeftY <= minOfRightX) {
+                if ((sum) % 2 == 0) {
+                    return ((Math.max(maxOfLeftX, maxOfLeftY) + Math.min(minOfRightX, minOfRightY))) / 2.0;
+                } else {
+                    return Math.min(minOfRightX, minOfRightY) + 0.0;
+                }
+            } else if (maxOfLeftX > minOfRightY) {
+                hi = x - 1;
+            } else
+                lo = x + 1;
+        }
+        throw new IllegalArgumentException();
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 3};
+        int[] nums2 = {2};
+        MedianOfTwoSortedArrays x = new MedianOfTwoSortedArrays();
+        System.out.print(x.findMedianSortedArrays(nums1, nums2));
+    }
+}
