d689b6565ed579716b5b74ca55c3cc565e3256f4
frankaowang@tencent.com <wa941121@163.com>
2019-04-16 16:48:35 +0800

    two_pointer

---
+++ src/Two_Pointers/MoveZeroes.java
@@ -0,0 +1,16 @@
+package Two_Pointers;
+
+public class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        if (nums == null || nums.length == 0) return;
+
+        int insertPos = 0;
+        for (int num : nums) {
+            if (num != 0) nums[insertPos++] = num;
+        }
+
+        while (insertPos < nums.length) {
+            nums[insertPos++] = 0;
+        }
+    }
+}
