9c8e6f52248bafff0b680a1f58707e58f466ea12
frankaowang@tencent.com <wa941121@163.com>
2019-09-04 16:57:20 +0800

    modify by leetcode changed

--- src/Array/MergeIntervals.java
+++ src/Array/MergeIntervals.java
@@ -1,21 +1,20 @@
 package Array;
 
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by wa941 on 2018/5/30.
  * 排序 collections.sort()
+ * 取右值要取两个区间的最大值，因为排序只按左值排列。
  */
 public class MergeIntervals {
-    private class myComparator implements Comparator<Interval> {
+
+    private class myComparator implements Comparator<int[]> {
         @Override
-        public int compare(Interval o1, Interval o2) {
-            if (o1.start < o2.start) {
+        public int compare(int[] o1, int[] o2) {
+            if (o1[0] < o2[0]) {
                 return -1;
-            } else if (o1.start > o2.start) {
+            } else if (o1[0] > o2[0]) {
                 return 1;
             } else {
                 return 0;
@@ -23,36 +22,29 @@ public class MergeIntervals {
         }
     }
 
-    public List<Interval> merge(List<Interval> intervals) {
-        List<Interval> result = new LinkedList<>();
-        Collections.sort(intervals, new myComparator());
-        for (Interval e : intervals) {
+    public int[][] merge(int[][] intervals) {
+        List<int[]> result = new LinkedList<>();
+        Arrays.sort(intervals, new myComparator());
+        for (int i = 0; i < intervals.length; i++) {
             if (result.isEmpty()) {
-                result.add(e);
+                result.add(intervals[i]);
             } else {
-                Interval x = result.get(result.size() - 1);
-                if (e.start > x.end) {
-                    result.add(e);
-                } else if (e.start >= x.start && e.start <= x.end) {
-                    x.end = Math.max(x.end, e.end);
+                if (((LinkedList<int[]>) result).getLast()[0] <= intervals[i][0] &&
+                        ((LinkedList<int[]>) result).getLast()[1] >= intervals[i][0]) {
+                    int[] x = {((LinkedList<int[]>) result).getLast()[0],
+                            Math.max(((LinkedList<int[]>) result).getLast()[1], intervals[i][1])};
+                    ((LinkedList<int[]>) result).removeLast();
+                    result.add(x);
+                } else {
+                    result.add(intervals[i]);
                 }
             }
         }
-        return result;
+        int[][] res = new int[result.size()][2];
+        for (int i = 0; i < result.size(); i++) {
+            res[i] = result.get(i);
+        }
+        return res;
     }
 }
 
-class Interval {
-    int start;
-    int end;
-
-    Interval() {
-        start = 0;
-        end = 0;
-    }
-
-    Interval(int s, int e) {
-        start = s;
-        end = e;
-    }
-}
