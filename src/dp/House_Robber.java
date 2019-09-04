b98b7dcc4169df04f58e9019f38aa8a4e08f4146
frankaowang@tencent.com <wa941121@163.com>
2019-09-04 10:41:34 +0800

    error fix

--- src/dp/House_Robber.java
+++ src/dp/House_Robber.java
@@ -2,7 +2,7 @@ package dp;
 
 /*
 max[i] 代表抢劫以i为结尾的房子的最大收益。 max[i] = Math.max(max[i-2]+nums[i] , max[i-1])
-起始条件 max[0] = nums[0] max[1] = nums[1]
+起始条件 max[0] = nums[0] Math.max(nums[0], nums[1])
  */
 public class House_Robber {
     public int rob(int[] nums) {
